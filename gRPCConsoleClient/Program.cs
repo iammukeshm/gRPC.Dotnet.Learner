cbcda6a0b9b6c20f590a16c6b1d8ede2e41e243e
Mukesh Murugan <iammukeshm@gmail.com>
2020-09-12 21:22:25 +0530

    implemented gRPC Client

--- gRPCConsoleClient/Program.cs
+++ gRPCConsoleClient/Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Grpc.Net.Client;
+using gRPCServer;
+using System;
+using System.Threading.Tasks;
 
 namespace gRPCConsoleClient
 {
-    class Program
+class Program
+{
+    static async Task Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        var data = new HelloRequest { Name = "Mukesh" };
+        var grpcChannel = GrpcChannel.ForAddress("https://localhost:5001");
+        var client = new Greeter.GreeterClient(grpcChannel);
+        var response = await client.SayHelloAsync(data);
+        Console.WriteLine(response.Message);
+        Console.ReadLine();
     }
+}
 }
