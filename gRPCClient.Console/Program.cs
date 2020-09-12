786117bb3e4a82a50e1b5d50ca732ab07e8874a6
Mukesh Murugan <iammukeshm@gmail.com>
2020-09-12 19:37:09 +0530

    add a console application as gRPC client

---
+++ gRPCClient.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gRPCClient.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- gRPCServer/Services/GreeterService.cs
+++ gRPCServer/Services/GreeterService.cs
@@ -1,26 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
 namespace gRPCServer
 {
-    public class GreeterService : Greeter.GreeterBase
+public class GreeterService : Greeter.GreeterBase
+{
+    private readonly ILogger<GreeterService> _logger;
+    public GreeterService(ILogger<GreeterService> logger)
     {
-        private readonly ILogger<GreeterService> _logger;
-        public GreeterService(ILogger<GreeterService> logger)
-        {
-            _logger = logger;
-        }
-
-        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
-        {
-            return Task.FromResult(new HelloReply
-            {
-                Message = "Hello " + request.Name
-            });
-        }
+        _logger = logger;
     }
+
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+}
 }
